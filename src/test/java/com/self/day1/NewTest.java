aba0ab02e21d72ea8777ddd2014df8a611a081be
Karthik Morab <mkarthik2299@gmail.com>
2021-10-12 16:09:37 +0530

    commited by stagging

---
+++ src/test/java/com/self/day1/NewTest.java
@@ -0,0 +1,11 @@
+package com.self.day1;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  System.out.println("TestNgDemo with Branching");
+  }
+  
+}
